2399d9ee7c9558cffd55db49293263fb144f3cc2
David Lanza <davidlanzadlv7@gmail.com>
2025-09-09 22:46:52 -0400

    prueba ec2user y ec2host

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -15,17 +15,70 @@ terraform {
     dynamodb_table = "REPLACE_ME_TABLE"       # Se reemplaza con -backend-config
     encrypt        = true
   }
-  
 }
 
 provider "aws" {
   region = var.aws_region
 }
 
+# --- VPC por defecto ---
+data "aws_vpc" "default" {
+  default = true
+}
+
+# --- Security Group MVP ---
+resource "aws_security_group" "mvp_sg" {
+  name        = "${var.project_name}-sg"
+  description = "SG MVP: SSH, HTTP, HTTPS abiertos"
+  vpc_id      = data.aws_vpc.default.id
+
+  # SSH abierto
+  ingress {
+    description = "SSH público"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP público
+  ingress {
+    description = "HTTP público"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTPS público
+  ingress {
+    description = "HTTPS público"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Todo permitido en salida
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-sg"
+  }
+}
+
+# --- EC2 usando el SG MVP ---
 resource "aws_instance" "this" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.mvp_sg.id]
 
   root_block_device {
     volume_size = var.volume_size
@@ -36,5 +89,3 @@ resource "aws_instance" "this" {
     Name = "${var.project_name}-ec2"
   }
 }
-
-
